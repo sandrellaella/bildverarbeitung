99b136f6e9cd0b26baeb8fcbccf97495fd0acc26
Sandra Schroeder <8schroed@informatik.uni-hamburg.de>
2013-03-03 21:16:12 +0100

    DFS fuer besseres skelett

--- src/DFS.py
+++ src/DFS.py
@@ -5,6 +5,8 @@ Created on Sun Mar  3 20:09:30 2013
 @author: sandra
 """
 
+import cv
+
 class Node:
     #Initialisierung der Knoten
     #Vorgaenger = NIL
@@ -43,33 +45,29 @@ def findNeighbours(node,nodes,searchDistance):
             y1 = n.feature[1]
             if x1>=x-searchDistance and x1<=x or x1<=x+searchDistance and x1>=x:
                 if y1>=y-searchDistance and y1<=y or y1<=y+searchDistance and y1>=y:        
-                    neighbours.append((x1,y1))
+                    neighbours.append(n)
     return neighbours
 
-def DFSVisit(node):
+def DFSVisit(node,nodes):
     node.setVisited()
     neighbours = findNeighbours(node,nodes,30)
     #print node.nodenumber, " : ", neighbours
     for neighbour in neighbours:
-        print neighbour
         if neighbour.visited is False:
+            #print neighbour.nodenumber, " : ", neighbour
             neighbour.predecessor = node
-            DFSVisit(neighbour)
-    
-
+            DFSVisit(neighbour,nodes)     
 
 def DFS(nodes):
     for i in range(len(nodes)):
         if nodes[i].visited == False:
-            DFSVisit(nodes[i])
+            DFSVisit(nodes[i],nodes)
             
 
     
-
-
 features = [(183.0, 225.0), (174.0, 177.0), (206.0, 245.0), (205.0, 185.0), (220.0, 176.0), (25.0, 224.0), (187.0, 191.0), (191.0, 170.0), (196.0, 235.0), (176.0, 160.0), (155.0, 168.0), (266.0, 128.0), (143.0, 25.0), (83.0, 49.0), (203.0, 71.0), (156.0, 93.0), (145.0, 44.0), (150.0, 68.0), (160.0, 109.0), (168.0, 132.0), (203.0, 133.0), (185.0, 213.0), (43.0, 227.0), (99.0, 250.0), (105.0, 97.0), (91.0, 69.0), (53.0, 231.0), (146.0, 250.0), (86.0, 245.0), (164.0, 122.0), (63.0, 235.0), (153.0, 81.0), (160.0, 243.0), (172.0, 149.0), (196.0, 154.0), (200.0, 144.0), (148.0, 58.0), (203.0, 116.0), (203.0, 94.0), (117.0, 252.0), (186.0, 201.0), (203.0, 106.0), (100.0, 87.0), (232.0, 170.0), (248.0, 154.0), (257.0, 142.0), (123.0, 132.0), (134.0, 150.0), (169.0, 235.0), (140.0, 158.0)]
 count = len(features)
-
+img = cv.LoadImage("hand.jpg")
 nodes = []
 
 #Zuordnung Features zu Knoten 
@@ -78,6 +76,6 @@ for i in range(count):
 
     
 DFS(nodes)
-    
+cv.Line(img, (int(current[0]),int(current[1])), (int(f[0]),int(f[1])), (0,255,0), thickness=1, lineType=8, shift=0)    
 
 
